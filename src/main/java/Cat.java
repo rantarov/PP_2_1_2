4f054b25ffcf107e06853268f3f89cb5add144e2
rantarov <92590848+rantarov@users.noreply.github.com>
2025-01-17 02:37:01 +0800

    complete task

---
+++ src/main/java/Cat.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+
+    public Cat() {
+        System.out.println("Cat constructor");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
